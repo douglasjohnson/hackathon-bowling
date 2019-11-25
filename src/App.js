08b3d0bc94d290c0092ed2530b164b4dba5aa525
douglasjohnson <douglas.johnson4@gmail.com>
2019-11-25 19:29:55 +0000

    Create skeleton test and app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import './App.css';
+import {TextField} from '@material-ui/core'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Hackathon Bowling v2 with feature
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    calculate = (value) => {
+        let output = value;
+        return output;
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: '',
+        };
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onChange = (event) => {
+        this.setState({
+            value: this.calculate(event.target.value)
+        });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">FizzBuzz</header>
+                <TextField id="input" variant="filled" type="number" label="Input" onChange={this.onChange}/>
+                <TextField id="output" variant="outlined" readOnly={true} value={this.state.value}/>
+            </div>
+        )
+    };
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,43 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {shallow} from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  let app;
+  let inputWrapper;
+
+  const changeInputValue = (value) => {
+    inputWrapper.simulate('change', {target: {value: value}});
+  };
+
+  const outputWrapper = () => {
+    return app.find('#output');
+  };
+
+  const outputValue = () => {
+    return outputWrapper().prop('value');
+  };
+
+  beforeEach(() => {
+    app = shallow(<App/>);
+    inputWrapper = app.find('#input');
+  });
+
+  it('should show header', () => {
+    expect(app.find('header').text()).toBe('FizzBuzz');
+  });
+  it('should show input text field', () => {
+    expect(inputWrapper.length).toBe(1);
+  });
+  it('should show output text field', () => {
+    expect(outputWrapper().length).toBe(1);
+  });
+
+  describe('should', function () {
+    it('return input value', function () {
+      changeInputValue(1);
+
+      expect(outputValue()).toBe(1);
+    });
+  });
 });
