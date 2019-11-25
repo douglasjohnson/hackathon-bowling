d66a9d737a4b040207ad281f057aa5996bbe4470
Douglas Johnson <douglas.johnson@verint.com>
2019-11-25 15:11:11 +0000

    v2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hackathon Bowling
+        Hackathon Bowling v2
       </header>
     </div>
   );
