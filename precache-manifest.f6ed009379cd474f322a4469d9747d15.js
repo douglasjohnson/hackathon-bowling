75b74e359f967ae7f89e911650d8eb4ced229c63
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-12 08:51:22 +0000

    Deploy douglasjohnson/hackathon-bowling to github.com/douglasjohnson/hackathon-bowling.git:gh-pages

---
+++ precache-manifest.f6ed009379cd474f322a4469d9747d15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1259ab81aaa2f7b5cf2a7ec27be2ed64",
+    "url": "./index.html"
+  },
+  {
+    "revision": "987df966cc51b9542a78",
+    "url": "./static/css/main.a21ae79a.chunk.css"
+  },
+  {
+    "revision": "196b8ed92b6818a2939f",
+    "url": "./static/js/2.0c08ed11.chunk.js"
+  },
+  {
+    "revision": "987df966cc51b9542a78",
+    "url": "./static/js/main.d79faaad.chunk.js"
+  },
+  {
+    "revision": "6264cef86fb40c318e88",
+    "url": "./static/js/runtime-main.85cdfc20.js"
+  }
+]);
